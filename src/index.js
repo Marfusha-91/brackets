4089930cfc6911bb301b5753ce4d8eeea1715b98
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-10 15:08:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  let char = str.split('');
-  stack = [],
-   open = ['{', '(', '['],
-   close = ['}', ')', ']'],
-   closeIndex,
-   openIndex;
-   for (let i = 0; len = char.length; i < len; i++) { openIndex = open.indexOf(char[i]);
-   if (openIndex !== -1) { stack.push(openIndex);   
-         continue;
-       }   
-   closeIndex = close.indexOf(char[i]);
-       if (closeIndex !== -1) {
-        openIndex = stack.pop(); 
-        if (closeIndex !== openIndex) {
-             return false;
-           }
-              }
-                  }
+  
+{
+    var chars = str.split(''),
+        stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        closeIndex,
+        openIndex;
 
- if (stack.length !== 0) {
+    for (var i = 0, len = chars.length; i < len; i++) {
+       openIndex = open.indexOf(chars[i]);
+       if (openIndex !== -1) {
+           stack.push(openIndex);
+           continue;
+       }
+
+       closeIndex = close.indexOf(chars[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+
+    if (stack.length !== 0) {
         return false;
     }
 
     return true;
-                  }
+}
