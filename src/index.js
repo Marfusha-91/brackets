001f57dd9d1004125934369e09fa911752cefdb2
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-10 14:36:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let char = str.split('');
+  stack = [],
+   open = ['{', '(', '['],
+   close = ['}', ')', ']'],
+   closeIndex,
+   openIndex;
+   for (let i = 0; len = char.lenth; i < len; i++) { openIndex open.indexOf(char[i]);
+   if (openIndex !== -1) stack.push(openIndex);
+           continue;
+       }   
+   closeIndex = close.indexOf(char[i]);
+       if (closeIndex !== -1) {
+        openIndex = stack.pop();
+        if (closeIndex !== openIndex) {
+             return false;
+           }
+       }
+    }
+
+ if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
+
+
 }
